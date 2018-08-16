2acbceb98931253b4c672e7f2001cbebd2854b91
Jitesh Kamble <jitesh@imgix.com>
2018-08-16 16:08:36 -0700

    Bump version to v2.1.1

--- src/Imgix/UrlBuilder.php
+++ src/Imgix/UrlBuilder.php
@@ -4,7 +4,7 @@ namespace Imgix;
 
 class UrlBuilder {
 
-    private $currentVersion = "2.1.0";
+    private $currentVersion = "2.1.1";
     private $domains;
     private $useHttps;
     private $signKey;
