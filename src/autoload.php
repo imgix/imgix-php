eac2cbd52485fb8db585387418441659214c0a79
Andréas Lundgren <andreas.lundgren@halle.se>
2022-11-23 13:58:52 +0100

    style: remove unused code

--- src/autoload.php
+++ src/autoload.php
@@ -14,7 +14,7 @@ spl_autoload_register(function ($className) {
     }
 
     $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $className . '.php';
-    //$fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $namespace . '.php';
+
     if (is_file($fileName)) {
         require $fileName;
 
